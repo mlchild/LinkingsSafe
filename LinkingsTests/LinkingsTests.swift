b737008fc2220d651184b3ab3c1008272942ccfd
Max Child <mlchild@gmail.com>
2016-01-12 16:01:40 -0800

    Initial Commit

---
+++ LinkingsTests/LinkingsTests.swift
@@ -0,0 +1,36 @@
+//
+//  LinkingsTests.swift
+//  LinkingsTests
+//
+//  Created by Max Child on 1/12/16.
+//  Copyright © 2016 Volley Inc. All rights reserved.
+//
+
+import XCTest
+@testable import Linkings
+
+class LinkingsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
